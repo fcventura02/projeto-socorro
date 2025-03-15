4bd488333c5699a56e29b85169d0dd12ac3a25d4
Filipe Cristiano <filipe.cristiano@uaify.com>
2025-03-15 15:59:24 -0300

    initial commit

---
+++ src/main.js
@@ -0,0 +1,17 @@
+document.querySelectorAll("instagram").forEach((element) => {element.innerHTML = `
+  <div>
+    <svg width="27" height="27" viewBox="0 0 27 27" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path d="M3.45349 7.22093C3.45349 5.14024 5.14024 3.45349 7.22093 3.45349H19.7791C21.8598 3.45349 23.5465 5.14024 23.5465 7.22093V19.7791C23.5465 21.8598 21.8598 23.5465 19.7791 23.5465H7.22093C5.14024 23.5465 3.45349 21.8598 3.45349 19.7791V7.22093Z" stroke="currentcolor" stroke-width="2"/>
+      <path d="M13.5 18.8372C16.4476 18.8372 18.8372 16.4476 18.8372 13.5C18.8372 10.5523 16.4476 8.16278 13.5 8.16278C10.5523 8.16278 8.16278 10.5523 8.16278 13.5C8.16278 16.4476 10.5523 18.8372 13.5 18.8372Z" stroke="currentcolor" stroke-width="2"/>
+      <path d="M19.7791 7.84883C20.1258 7.84883 20.407 7.56771 20.407 7.22092C20.407 6.87414 20.1258 6.59302 19.7791 6.59302C19.4323 6.59302 19.1512 6.87414 19.1512 7.22092C19.1512 7.56771 19.4323 7.84883 19.7791 7.84883Z" fill="currentcolor"/>
+    </svg> 
+  </div>
+`});
+document.querySelectorAll("share").forEach((element) => {element.innerHTML = `
+  <div>
+    <svg width="27" height="27" viewBox="0 0 27 27" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path d="M22.9186 14.1279V22.2907C22.9186 22.9843 22.3564 23.5465 21.6628 23.5465H4.70931C4.01572 23.5465 3.45349 22.9843 3.45349 22.2907V5.3372C3.45349 4.64362 4.01572 4.08139 4.70931 4.08139H12.8721" stroke="currentcolor" stroke-width="2" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
+      <path d="M23.5465 3.45349H15.6977M23.5465 3.45349V11.3023M23.5465 3.45349L13.5 13.5" stroke="#AD2E32" stroke-width="3" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
+    </svg>
+  </div>
+`});
