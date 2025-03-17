8390675729346408de9b4b77b3eafc43de55b426
Filipe Cristiano <filipe.cristiano@uaify.com>
2025-03-17 09:15:17 -0300

    Updated links and favicon

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-document.querySelectorAll("instagram").forEach((element) => {element.innerHTML = `
+(document.querySelectorAll("instagram").forEach((element) => {element.innerHTML = `
   <div>
     <svg width="27" height="27" viewBox="0 0 27 27" fill="none" xmlns="http://www.w3.org/2000/svg">
       <path d="M3.45349 7.22093C3.45349 5.14024 5.14024 3.45349 7.22093 3.45349H19.7791C21.8598 3.45349 23.5465 5.14024 23.5465 7.22093V19.7791C23.5465 21.8598 21.8598 23.5465 19.7791 23.5465H7.22093C5.14024 23.5465 3.45349 21.8598 3.45349 19.7791V7.22093Z" stroke="currentcolor" stroke-width="2"/>
@@ -6,7 +6,7 @@ document.querySelectorAll("instagram").forEach((element) => {element.innerHTML =
       <path d="M19.7791 7.84883C20.1258 7.84883 20.407 7.56771 20.407 7.22092C20.407 6.87414 20.1258 6.59302 19.7791 6.59302C19.4323 6.59302 19.1512 6.87414 19.1512 7.22092C19.1512 7.56771 19.4323 7.84883 19.7791 7.84883Z" fill="currentcolor"/>
     </svg> 
   </div>
-`});
+`}),
 document.querySelectorAll("share").forEach((element) => {element.innerHTML = `
   <div>
     <svg width="27" height="27" viewBox="0 0 27 27" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -14,4 +14,4 @@ document.querySelectorAll("share").forEach((element) => {element.innerHTML = `
       <path d="M23.5465 3.45349H15.6977M23.5465 3.45349V11.3023M23.5465 3.45349L13.5 13.5" stroke="#AD2E32" stroke-width="3" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
     </svg>
   </div>
-`});
+`}))()
